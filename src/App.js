85c56f32b038f017104a59c0b5a845989dc7e9b9
abhinav <abhinavdev451@gmail.com>
2019-10-23 11:26:04 +0530

    - updated changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Abhinav Gundla
-        </p>
-      </header>
+    <div className="App"> 
+        <div class="card">
+          <div class="me">
+            <h1>Abhinav Gundla</h1>
+            <h2>Javascript Engineer, Full Stack Developer</h2>
+            <span class="and">&amp;</span>
+            <h2>Angular Enthusiast</h2>
+          </div>
+
+          <div class="contact">
+            <p>abhinavdev451@gmail.com</p>
+          </div>
+        </div>
     </div>
   );
 }
